b09cc7cfe2a89c6dece39498722d78f7926d2ec0
libra625 <18.12.17.moyo@gmail.com>
2025-03-29 18:25:32 +0200

    updated

--- app/src/main/java/com/example/lr1/MainActivity.kt
+++ app/src/main/java/com/example/lr1/MainActivity.kt
@@ -26,53 +26,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize views
         scrambledWordTextView = findViewById(R.id.scrambledWord)
         userInputEditText = findViewById(R.id.userInput)
         submitButton = findViewById(R.id.submitButton)
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            // Create an Intent to start the SecondActivity
             val intent = Intent(this, BirthdayActivity::class.java)
-            startActivity(intent)  // Start the activity
+            startActivity(intent)
         }
 
-        // Set up the game
         startNewGame()
 
-        // Set click listener for submit button
         submitButton.setOnClickListener {
             val userGuess = userInputEditText.text.toString().trim()
             if (userGuess.equals(currentWord, ignoreCase = true)) {
-                // Show a Toast message for correct guess
                 Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
-                startNewGame() // Start a new round
+                startNewGame()
             } else {
-                // Show a Toast message for incorrect guess
                 Toast.makeText(this, "Incorrect! Try again.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    // Start a new game by selecting a random word and scrambling it
     private fun startNewGame() {
-        // Check if availableWords is empty, refill it if necessary
         if (availableWords.isEmpty()) {
-            availableWords = words.toMutableList() // Refill with the original list
+            availableWords = words.toMutableList()
         }
 
-        // Select a random word from the availableWords list
         currentWord = availableWords.random()
 
-        // Scramble the word
         scrambledWord = currentWord.toCharArray().apply { shuffle() }.concatToString()
 
-        // Update UI with scrambled word
         scrambledWordTextView.text = scrambledWord
         userInputEditText.text.clear()
 
-        // Remove the chosen word from the availableWords list
         availableWords.remove(currentWord)
     }
 }
